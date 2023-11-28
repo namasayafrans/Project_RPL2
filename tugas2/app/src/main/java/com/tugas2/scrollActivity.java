d425a552c6950028595d530886e1e00d60302a70
namasayafrans <sinagafrans38@gmail.com>
2023-11-28 09:11:37 +0700

    rpl2

---
+++ tugas2/app/src/main/java/com/tugas2/scrollActivity.java
@@ -0,0 +1,15 @@
+package com.tugas2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class scrollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll);
+
+    }
+}
